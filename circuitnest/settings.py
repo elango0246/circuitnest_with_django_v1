e42f4ea5feb9d8ecd00fd0bf74b11dc96f96acbe
Elango <elango0246@gmail.com>
2026-08-25 16:01:26 +0530

    databse 2

--- circuitnest/settings.py
+++ circuitnest/settings.py
@@ -83,14 +83,24 @@ WSGI_APPLICATION = 'circuitnest.wsgi.application'
 # --------------------------------------------------
 # DATABASE
 # --------------------------------------------------
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
-        conn_max_age=600,
-        ssl_require=True,
-    )
-}
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True,
+        )
+    }
+else:
+    # Local development
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # --------------------------------------------------
