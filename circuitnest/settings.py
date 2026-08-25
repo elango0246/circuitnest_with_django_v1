bc764aa129e6923cf49b349fd39b4b6adfb43813
Elango <elango0246@gmail.com>
2026-08-25 15:54:48 +0530

    database

--- circuitnest/settings.py
+++ circuitnest/settings.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-
+import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # --------------------------------------------------
@@ -85,10 +85,11 @@ WSGI_APPLICATION = 'circuitnest.wsgi.application'
 # --------------------------------------------------
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 
